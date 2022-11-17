95454dc685abe2e103638c1c7b4a37e680c7aa28
SakuOrdrTab <113209622+SakuOrdrTab@users.noreply.github.com>
2022-11-17 20:49:42 +0200

    Add files via upload

---
+++ Ball.py
@@ -0,0 +1,34 @@
+# Ball object class defining python file
+# Ver 0.01
+
+FRAME_MIN_X = 1
+FRAME_MAX_X = 500
+FRAME_MIN_Y = 1
+FRAME_MAX_Y = 500
+
+import random
+
+class Ball:
+    
+    radius = 1
+    x = 0
+    y = 0
+    x_vel = 0.0
+    y_vel = 0.0
+    mass = 100
+    
+    def __init__(self):
+        self.x = random.randint(FRAME_MIN_X, FRAME_MAX_X)
+        self.y = random.randint(FRAME_MIN_Y, FRAME_MAX_Y)
+        self.radius = random.randint(10,50)
+        self.mass = int(self.radius*self.radius*3.14)
+        self.x_vel = random.random() * 5
+        self.y_vel = random.random() * 5
+        return None
+    
+    
+    
+    
+    
+random.seed()
+print("Randomization complete.")
---
+++ bouncing_balls.py
@@ -0,0 +1,25 @@
+# Ver 0.01
+
+import Ball
+
+def main():
+    # create balls
+    # loop:
+    #   move balls
+    #   bounce balls
+    #   draw balls
+    #   wait to 10 ms
+    # exit  
+    
+    # debug:
+    ball = Ball.Ball()
+    ball2 = Ball.Ball()
+    ball2.mass = 3000
+    print(ball.radius)
+    print(ball.mass)
+    print(ball2.radius)
+    print(ball2.mass)
+    
+    return None
+
+main()
